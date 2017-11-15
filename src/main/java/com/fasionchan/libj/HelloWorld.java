45ad532c788d7636203b310003d9182431f4ab3f
Fasion Chan <fasionchan@gmail.com>
2017-11-15 19:18:04 +0800

    HelloWorld

---
+++ src/main/java/com/fasionchan/libj/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.fasionchan.libj;
+
+
+public class HelloWorld {
+    private static void sayHelloWorld() {
+        System.out.println("Hello, world!");
+    }
+
+    public static void main(String[] args) {
+        sayHelloWorld();
+    }
+}
